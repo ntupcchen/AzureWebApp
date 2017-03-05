e7c73d671b780c8e74da92e31b7b213c5d1f7516
ntupcchen <r05922125@ntu.edu.tw>
2017-03-05 19:38:01 +0800

    For Azure

---
+++ server.js
@@ -0,0 +1,17 @@
+// 引用 http 模組
+var http = require('http')
+
+// 設定 port 預設為 1337，若系統環境有設定則以系統環境設定為主
+var port = process.env.PORT || 1337;
+
+port =80;
+
+// 建立一個 http server
+var app = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(port*2+'\n');
+})
+
+// 啟動並等待連接
+app.listen(port);
+console.log('Server running on port ' + port);
